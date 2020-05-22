dfb68005a905d3ddadc1d79b71f3c1a43e2a5749
Artur Skowronski <me@arturskowronski.com>
2020-05-22 22:27:48 +0200

    Hints for shell

--- run.sh
+++ run.sh
@@ -42,6 +42,27 @@ git config --global core.excludesfile '~/.gitignore'
 git config --global user.name "Artur Skowronski"
 git config --global user.email "me@arturskowronski.com"
 
+defaults write com.apple.Dock showhidden -bool YES && killall Dock
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+defaults write com.apple.dock tilesize -int 36
+defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+defaults write com.apple.dock show-process-indicators -bool true
+defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.dock dashboard-in-overlay -bool true
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock showhidden -bool true
+defaults write com.apple.dock show-recents -bool false
+
+
+
 # echo "  Easter Egg seals are happy                                           ";
 # echo "          (\/ )        _  _         ( \/)                              ";
 # echo "           \  |       ( \/ )        |  /                               ";
