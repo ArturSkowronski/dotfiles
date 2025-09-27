1c5de1f85200c4131dfd130018e9ca1f26ebe749
Artur Skowronski <me@arturskowronski.com>
2025-09-27 14:47:10 +0200

    Update gcloud and pnpm scripts to use HOME variable for paths

--- shell/init/gcloud.sh
+++ shell/init/gcloud.sh
@@ -1,5 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/askowronski/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/askowronski/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '$HOME/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/Downloads/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/askowronski/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/askowronski/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '$HOME/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
--- shell/init/pnpm.sh
+++ shell/init/pnpm.sh
@@ -1,5 +1,7 @@
 # pnpm
-export PNPM_HOME="/Users/askowronski/Library/pnpm"
+PNPM_HOME="$HOME/Library/pnpm"
+if ! [ -d "$PNPM_HOME" ]; then return; fi
+export PNPM_HOME
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
